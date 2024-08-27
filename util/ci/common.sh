ca357ce835432426b6899e65f4df59990825a2be
SFENCE <sfence.software@gmail.com>
2024-08-27 19:42:14 +0200

    Build from sources with prebuilded dependencies. Cross build for x86_64 and older MacOSX SDKs.

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -26,7 +26,7 @@ install_linux_deps() {
 }
 
 # macOS build only
-install_macos_deps() {
+install_macos_brew_deps() {
 	local pkgs=(
 		cmake gettext freetype gmp jpeg-turbo jsoncpp leveldb
 		libogg libpng libvorbis luajit zstd
@@ -38,4 +38,23 @@ install_macos_deps() {
 	brew install --display-times "${pkgs[@]}"
 	brew unlink $(brew ls --formula)
 	brew link "${pkgs[@]}"
+}
+
+install_macos_precompiled_deps() {
+  osver=$1
+	arch=$2
+
+  local pkgs=(
+    cmake wget
+  )
+  export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+  export HOMEBREW_NO_INSTALL_CLEANUP=1
+  # contrary to how it may look --auto-update makes brew do *less*
+  brew update --auto-update
+  brew install --display-times "${pkgs[@]}"
+  brew unlink $(brew ls --formula)
+  brew link "${pkgs[@]}"
+
+  wget macos${osver}_${arch}_deps.tar.gz https://github.com/luanti-org/luanti_macos_deps/releases/download/latest/macos${osver}_${arch}_deps.tar.gz || echo "Ignore stupid error number 4: $?"
+  tar -xf macos${osver}_${arch}_deps.tar.gz
 }
