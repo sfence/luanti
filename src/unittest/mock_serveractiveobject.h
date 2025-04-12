aa52cec680b15e63c722f4443a5980018e0c9469
Lars Mueller <appgurulars@gmx.de>
2025-04-12 20:22:46 +0200

    Implement getGUID properly for MockSAOs

--- src/unittest/mock_serveractiveobject.h
+++ src/unittest/mock_serveractiveobject.h
@@ -3,17 +3,30 @@
 // Copyright (C) 2022 Minetest core developers & community
 
 #include "guid.h"
+#include "serverenvironment.h"
 #include <server/serveractiveobject.h>
+#include <string>
 
 class MockServerActiveObject : public ServerActiveObject
 {
 public:
 	MockServerActiveObject(ServerEnvironment *env = nullptr, v3f p = v3f()) :
-		ServerActiveObject(env, p) {}
+		ServerActiveObject(env, p)
+	{
+		if (env)
+			m_guid = env->getGUIDGenerator().next().hex();
+	}
 
 	virtual ActiveObjectType getType() const { return ACTIVEOBJECT_TYPE_TEST; }
 	virtual bool getCollisionBox(aabb3f *toset) const { return false; }
 	virtual bool getSelectionBox(aabb3f *toset) const { return false; }
 	virtual bool collideWithObjects() const { return false; }
-	virtual const std::string& getGUID() { assert(false); }
+	virtual const std::string& getGUID()
+	{
+		assert(!m_guid.empty());
+		return m_guid;
+	}
+
+private:
+	std::string m_guid;
 };
