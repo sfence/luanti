minetest/minetest

Move inner_node definition to metadata. Update doc, remove code duplications in content_mapblock.cpp.

src/nodemetadata.h
@@ -21,6 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include <unordered_set>
 #include "metadata.h"
+#include "nodedef.h"
 
 /*
 	NodeMetadata stores arbitary amounts of data for special blocks.
@@ -65,12 +66,20 @@ private:
 	std::unordered_set<std::string> m_privatevars;
 };
 
+struct RenderCachedMetadata
+{
+	// sunken/covered
+	content_t inner_id;
+	u8 inner_param2;
+};
+
 
 /*
 	List of metadata of all the nodes of a block
 */
 
 typedef std::map<v3s16, NodeMetadata *> NodeMetadataMap;
+typedef std::map<v3s16, RenderCachedMetadata> RenderCachedMetadataMap;
 
 class NodeMetadataList
 {
@@ -109,9 +118,24 @@ public:
 		return m_data.end();
 	}
 
+#ifndef SERVER // Only on client
+	// return copy of render cache
+	RenderCachedMetadataMap getRenderCache() { return m_render_cache; }
+	// clear render cache
+	void clearRenderCache() { m_render_cache.clear(); }
+	// Deletes old data and sets a new one with render cache update
+	void set(v3s16 p, NodeMetadata *d, const ContentFeatures *f, const NodeDefManager *ndef);
+	// set render cache for data
+	void setRenderCache(v3s16 p, const ContentFeatures *f, const NodeDefManager *ndef);
+private:
+	void setRenderCacheRaw(v3s16 p, NodeMetadata *d, const ContentFeatures *f, const NodeDefManager *ndef);
+#endif
 private:
 	int countNonEmpty() const;
 
 	bool m_is_metadata_owner;
 	NodeMetadataMap m_data;
+#ifndef SERVER // Only on client
+	RenderCachedMetadataMap m_render_cache;
+#endif
 };

sfence/luanti

src/nodemetadata.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <memory>
 #include "metadata.h"
+#include "mapnode.h"
 
 /*
 	NodeMetadata stores arbitary amounts of data for special blocks.
@@ -20,6 +21,8 @@
 
 class Inventory;
 class IItemDefManager;
+class NodeDefManager;
+struct ContentFeatures;
 
 class NodeMetadata : public SimpleMetadata
 {
@@ -55,12 +58,20 @@ private:
 	std::unordered_set<std::string> m_privatevars;
 };
 
+struct RenderCachedMetadata
+{
+	// sunken/covered
+	content_t inner_id;
+	u8 inner_param2;
+};
+
 
 /*
 	List of metadata of all the nodes of a block
 */
 
 typedef std::map<v3s16, NodeMetadata *> NodeMetadataMap;
+typedef std::map<v3s16, RenderCachedMetadata> RenderCachedMetadataMap;
 
 class NodeMetadataList
 {
@@ -99,9 +110,24 @@ public:
 		return m_data.end();
 	}
 
+#ifndef SERVER // Only on client
+	// return copy of render cache
+	RenderCachedMetadataMap getRenderCache() { return m_render_cache; }
+	// clear render cache
+	void clearRenderCache() { m_render_cache.clear(); }
+	// Deletes old data and sets a new one with render cache update
+	void set(v3s16 p, NodeMetadata *d, const ContentFeatures *f, const NodeDefManager *ndef);
+	// set render cache for data
+	void setRenderCache(v3s16 p, const ContentFeatures *f, const NodeDefManager *ndef);
+private:
+	void setRenderCacheRaw(v3s16 p, NodeMetadata *d, const ContentFeatures *f, const NodeDefManager *ndef);
+#endif
 private:
 	int countNonEmpty() const;
 
 	bool m_is_metadata_owner;
 	NodeMetadataMap m_data;
+#ifndef SERVER // Only on client
+	RenderCachedMetadataMap m_render_cache;
+#endif
 };